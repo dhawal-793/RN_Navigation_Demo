52284097d9d8af5e431126fec5cdf5f75fdfd5b5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-23 22:48:52 +0530

    Implemented Navigation inside App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+
+// Types for the navigation
+export type RootStackParamList = {
+  Home: undefined;
+  Details: { ProductId: string };
+};
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>React Native Navigation Demo</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Overview'
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            title: 'Product Details'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({});
 
 export default App;
